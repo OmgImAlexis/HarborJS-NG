f5ef3e9b1949ba65a9e3fd36729c7c5e6b421475
Stanislas Leduc <stanislas.leduc@balinor.net>
2017-05-20 19:46:18 +0200

    Update code for integration with Dokku

--- config/application.js
+++ config/application.js
@@ -3,7 +3,7 @@ var App       = require('../app/models/apps');
 
 exports.create = function(req, res){
   new App({
-	name : 'app/'+req.body.name+':latest',
+	name : 'dokku/'+req.body.name+':latest',
         user : req.body.user
   }).save(function(err, app, count){
     res.redirect('/new');
